3ec52ca06cb05a4a3c0115a68cb6a2b12b60e470
Juan Diego <juan.gonzalezgerman@mavs.uta.edu>
2018-10-12 23:31:04 -0500

    removed mavshell src files, as they have been moved to a proper subdir

--- doorknobs.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("Remember, it is illegal to lick doorknobs on some planets\n");
-	return 0;
-}
--- loop.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sys/wait.h>
-
-int main()
-{
-	sleep(10);	
-
-	return 0;
-}
